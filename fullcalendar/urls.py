3d33a6165653713f61d01d2d2a3e1021fcb1bcdf
Nurin Izzati Jafri <nurinizzatijafri@gmail.com>
2020-08-17 00:04:47 +0900

    can delete without refresh

--- fullcalendar/urls.py
+++ fullcalendar/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'fullcalendar'
 urlpatterns = [
     url('^calendar', views.calendar, name='calendar'),
-    url('^add_event$', views.add_event, name='add_event'),
-    url('^update$', views.update, name='update'),
+    url('^add_event', views.add_event, name='add_event'),
+    url('^update', views.update, name='update'),
     url('^remove', views.remove, name='remove'),
 ]
